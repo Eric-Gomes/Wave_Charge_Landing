6fdae29cf6a889d105597b845f9c85e3fbb73886
Eric Gomes <eric.n.gomes@hotmail.com>
2022-05-30 16:58:41 +0100

    Changes to footer, html, css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
