aa0831a5dc1a2dc5e9a2ac2a3d2550e3610e45a2
Eric Gomes <eric.n.gomes@hotmail.com>
2022-05-31 05:52:52 +0100

    Minor changes to HTML elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IntroComponent } from './intro/intro.component';
-import { FooterComponent } from './footer/footer.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { StoryComponent } from './Components/story/story.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
     FooterComponent,
+    StoryComponent,
   ],
   imports: [
     BrowserModule,
